6ca642d7a4fccb289606ca99bece6868366d1219
Aharnish <aharnishpithva@gmail.com>
2023-05-30 15:22:53 +0530

    init

---
+++ modules/layer.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+#define telemetry 1
+#define fori(i,n) for(int i = 0; i < n; i++)
+#define pb push_back
+
+namespace std
+{
+    
+    class nlayer{
+    
+        public:
+
+        __UINT16_TYPE__ n;
+
+        // shape of this 3D layer
+        __UINT16_TYPE__ x;
+        __UINT16_TYPE__ y;
+        __UINT16_TYPE__ z;
+
+        
+
+
+        
+
+
+
+    }
+
+
+} // namespace std
+
+
