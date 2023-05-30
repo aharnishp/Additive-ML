95b416fca5d68f3745a46795812f8f17ff85d6bc
Aharnish <aharnishpithva@gmail.com>
2023-05-30 18:42:40 +0530

    init1

--- modules/layer.cpp
+++ modules/layer.cpp
@@ -12,12 +12,25 @@ namespace std
     
         public:
 
-        __UINT16_TYPE__ n;
+        __UINT32_TYPE__ id = 0;
 
         // shape of this 3D layer
         __UINT16_TYPE__ x;
         __UINT16_TYPE__ y;
         __UINT16_TYPE__ z;
+
+        // vector of pointers storing pointer to input layers
+        vector<int*> input_layers(0, nullptr);
+
+        // 3D vector storing weights
+        vector<vector<vecctor<float>>> weights;
+
+        // float storing bias
+        float bias;
+
+        
+
+
 
         
 
